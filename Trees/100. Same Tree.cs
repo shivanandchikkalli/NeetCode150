4b8640084644761cd8b9b0ce79ff2b97e640664e
Shivanand Chikkalli <sychikkalli@gmail.com>
2026-02-08 11:22:12 +0530

    100. Same Tree

---
+++ Trees/100. Same Tree.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p is null && q is null)
+            return true;
+        else if((p is null && q is not null) || (p is not null && q is null))
+            return false;
+
+        if(p.val != q.val)
+            return false;
+
+        bool isLeftSameTree = IsSameTree(p.left, q.left);
+        bool isRightSameTree = IsSameTree(p.right, q.right);
+
+        return isLeftSameTree && isRightSameTree;
+    }
+}
