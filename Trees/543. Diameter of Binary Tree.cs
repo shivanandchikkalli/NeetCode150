326f51aeebf7fd22a67ab238be8e2d76410f66cc
Shivanand Chikkalli <sychikkalli@gmail.com>
2026-02-08 11:02:20 +0530

    543. Diameter of Binary Tree

---
+++ Trees/543. Diameter of Binary Tree.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int DiameterOfBinaryTree(TreeNode root) {
+        int diameter = 0;
+        int MaxDepth(TreeNode root) {
+            if(root is null)
+                return 0;
+
+            if(root.left is null && root.right is null)
+                return 1;
+            
+            var leftDepth = MaxDepth(root.left);
+            var rightDepth = MaxDepth(root.right);
+
+            diameter = Math.Max(diameter, leftDepth + rightDepth);
+
+            return 1 + Math.Max(leftDepth, rightDepth); 
+        }
+
+        MaxDepth(root);
+
+        return diameter;         
+    }
+}
